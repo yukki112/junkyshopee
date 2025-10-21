8587e923c6d4a18ecae7d15f00a26f6311c1089e
yukki112 <stephenviray12@gmail.com>
2025-10-21 14:48:36 +0800

    ILY

--- Employee/api/api_customers_simple.php
+++ Employee/api/api_customers_simple.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET');
 header('Access-Control-Allow-Headers: Content-Type');
 
-require_once 'db_connection.php';
+require_once '../db_connection.php';
 
 try {
     $query = "SELECT 
